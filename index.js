8fa0d10d50a8d744af75bd521238b689edad11a8
rohith <rohith@persona3.io>
2025-03-10 15:41:24 +0530

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const PORT = 4000; // Ensure B.com runs on this port
+
+app.use(cookieParser());
+
+// CORS setup to allow A.com to make requests
+app.use(
+  cors({
+    origin: "https://demo-adv-tau.vercel.app/", // Allow A.com to send requests
+    credentials: true, // Allow cookies to be sent
+  })
+);
+
+// API to set a cookie
+app.get("/set-cookie", (req, res) => {
+  res.cookie("test_cookie", "HelloFromB", {
+    path: "/",
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+  });
+
+  res.json({ message: "Cookie set from B.com!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server running on https://B.com:${PORT}`);
+});
